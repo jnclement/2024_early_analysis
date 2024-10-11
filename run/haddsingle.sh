045d41ffcba51d7c33fcb446dd38259db2317026
Joseph Clement <jocl6895@colorado.edu>
2024-10-11 16:18:31 -0400

    forgot haddsingle

--- run/haddsingle.sh
+++ run/haddsingle.sh
@@ -4,4 +4,4 @@
 source /opt/sphenix/core/bin/sphenix_setup.sh -n
 source /opt/sphenix/core/bin/setup_local.sh "/sphenix/user/jocl/projects/testinstall"
 export HOME=/sphenix/u/jocl
-hadd -f "output/sumroot/summed_${1}_${2}_base.root" `ls output/root/*${1}*${2}*fullfile*`
+hadd -f "output/sumroot3/summed_${1}_${2}_base.root" `ls output/root3/*${1}*${2}*fullfile*`
